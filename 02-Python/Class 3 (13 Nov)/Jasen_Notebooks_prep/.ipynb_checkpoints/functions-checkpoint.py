58ccb4b5e4ea709a7ba1b0ef4c38c460d076f3ba
Jasen Mackie <jmackie@questrade.com>
2021-11-16 17:41:06 -0500

    remove solutions, add prep notebooks for 3.2

---
+++ 02-Python/Class 3 (13 Nov)/Jasen_Notebooks_prep/.ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,8 @@
+def print_hello():
+    print("hello!")
+
+def print_goodbye():
+    print("goodbye!")
+    
+def print_new_string():
+    print("Fintech is great")
--- 02-Python/Class 3 (13 Nov)/debug_demo.py
+++ 02-Python/Class 3 (13 Nov)/debug_demo.py
@@ -2,57 +2,66 @@
 from pathlib import Path
 import csv
 
-# Set the file path
-csvpath = 'C:/Users/013769/GitHub/UniTor/Week 2 - Python/Resources/sales.csv'
+# Define a function for adding 2 variables
+def add(param1, param2):
+    print(param1)
+    print(param2)
+    return param1 + param2
+    
 
-# Initialize list of records
-records = []
+add(1,2)
 
-# Open the csv file as an object
-with open(csvpath, 'r') as csvfile:
+# # Set the file path
+# csvpath = 'C:/Users/013769/GitHub/UniTor/UTOR-VIRT-FIN-PT-11-2021-U-C/02-Python/Class 3 (13 Nov)/Resources/sales.csv'
 
-    # Pass in the csv file to the csv.reader() function
-    # (with ',' as the delmiter/separator) and return the csvreader object
-    csvreader = csv.reader(csvfile, delimiter=',')
+# # Initialize list of records
+# records = []
 
-    # Read the header row first (skip this step if there is no header)
-    csv_header = next(csvreader)
-    # Print the header
-    print(csv_header)
+# # Open the csv file as an object
+# with open(csvpath, 'r') as csvfile:
 
-    # Append the column 'Average' to the header
-    csv_header.append("Average")
-    # Append the header to the list of records
-    records.append(csv_header)
+#     # Pass in the csv file to the csv.reader() function
+#     # (with ',' as the delmiter/separator) and return the csvreader object
+#     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read each row of data after the header
-    for row in csvreader:
-        # Print the row
-        print(row)
+#     # Read the header row first (skip this step if there is no header)
+#     csv_header = next(csvreader)
+#     # Print the header
+#     print(csv_header)
 
-        # Set the 'name', 'count', and 'revenue' variables for better
-        # readability, convert strings to ints for numerical calculations
-        name = row[0]
-        count = int(row[1])
-        revenue = int(row[2])
+#     # Append the column 'Average' to the header
+#     csv_header.append("Average")
+#     # Append the header to the list of records
+#     records.append(csv_header)
 
-        # Calculate the average and round to the nearest 2 decimal places
-        average = round(revenue / count, 2)
+#     # Read each row of data after the header
+#     for row in csvreader:
+#         # Print the row
+#         print(row)
 
-        # Append the average to the row
-        row.append(average)
-        # Append the row to the list of records
-        records.append(row)
+#         # Set the 'name', 'count', and 'revenue' variables for better
+#         # readability, convert strings to ints for numerical calculations
+#         name = row[0]
+#         count = int(row[1])
+#         revenue = int(row[2])
 
-# Set the path for the output.csv
-output = Path("output.csv")
+#         # Calculate the average and round to the nearest 2 decimal places
+#         average = round(revenue / count, 2)
 
-# Open the output path as a file and pass into the 'csv.writer()' function
-# Set the delimiter/separater as a ','
-with open(output, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',')
+#         # Append the average to the row
+#         row.append(average)
+#         # Append the row to the list of records
+#         records.append(row)
 
-    # Loop through the list of records and write every record to the
-    # output csv file
-    for record in records:
-        csvwriter.writerow(record)
+# # Set the path for the output.csv
+# output = Path("output.csv")
+
+# # Open the output path as a file and pass into the 'csv.writer()' function
+# # Set the delimiter/separater as a ','
+# with open(output, 'w') as csvfile:
+#     csvwriter = csv.writer(csvfile, delimiter=',')
+
+#     # Loop through the list of records and write every record to the
+#     # output csv file
+#     for record in records:
+#         csvwriter.writerow(record)
